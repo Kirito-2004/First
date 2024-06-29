f1468588da782b25a90a8433e94f86d6ebfb56c2
Hius <laquanghieuvh@gmail.com>
2024-06-29 11:51:38 +0200

    Add Bill, Information, ListBill class 11:51M 29/6/2024

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,32 @@
 public class Product {
+    private int idProd;
+    private String name;
+    private double price;
 
+    public Product(int idProd, String name, double price){
+        this.idProd = idProd;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setIdProd(int idProd){
+        this.idProd = idProd;
+    }
+    public int getIdProd(){
+        return idProd;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public double getPrice(){
+        return price;
+    }
 }
