b8d2deeadfe718d5703de0ab846fbd70e6ebede6
Hius <laquanghieuvh@gmail.com>
2024-06-29 11:25:22 +0200

    Add class Detail, Cart, Product 11:25PM 29/6/2024

---
+++ src/Cart.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Cart {
+    private ArrayList<Detail> listDetail;
+
+    public Cart(){
+        listDetail = new ArrayList<Detail>();
+    }
+}
---
+++ src/Detail.java
@@ -0,0 +1,23 @@
+public class Detail {
+    private Product product;
+    private int qty;
+
+    public Detail(Product product, int qty){
+        this.product = product;
+        this.qty = qty;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+    public void setProduct(Product product){
+        this.product = product;
+    }
+
+    public int getQty(){
+        return qty;
+    }
+    public void setQty(int qty){
+        this.qty = qty;
+    }
+}
