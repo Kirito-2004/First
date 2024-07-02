4c2b54084cca9544c43b0e3d1102a2ebfb5bf273
SilentBoy2004 <huutho846oz@gmail.com>
2024-07-02 11:16:37 +0700

    Update login

--- src/Account.java
+++ src/Account.java
@@ -2,5 +2,18 @@ public class Account {
     private int idAcc;
     private int tel;
     private String password;
+    private Customer customer;
+    public Account(){}
+    public Account(String name, int tel, String password){
+        Customer c = new Basic(name,0);
+        this.idAcc=Sever.getLastAcc();
+        Sever.setLastAcc(Sever.getLastAcc()+1);
+        this.tel=tel;
+        this.password=password;
+        this.customer=c;
+    }
 
+    public String getPassword() {
+        return password;
+    }
 }
--- src/Sever.java
+++ src/Sever.java
@@ -3,27 +3,39 @@ import java.util.Scanner;
 public class Sever {
     private static int lastAcc;
     private HashMap<Integer,Account> hashAcc;
-
+    public Sever(){
+        this.hashAcc = new HashMap<>();
+        Sever.lastAcc=0;
+    }
     public void createAccount(){
         boolean flag = false;
-        System.out.println("------------------------------");
-        System.out.println("#Create Account:");
+        System.out.println("\u001B[36m"+"#Create Account:"+"\u001B[0m");
         System.out.print("Name: ");
         String name = this.nextLine();
+        int tel=0;
         while (!flag){
             try {
                 System.out.print("Tel: ");
-                int tel = this.nextInt();
+                tel = this.nextInt();
                 flag=true;
             }
             catch (Exception ex){
-                System.out.println("\u001B[31m" + "<Tel invalid, please try again!>"+"\u001B[0m");
+                System.out.println("\u001B[31m" + "<Invalid phone number, please try again!>"+"\u001B[0m");
             }
         }
         System.out.print("Password: ");
         String password = this.nextLine();
-        System.out.println("\u001B[32m" + "<Create account successful>"+"\u001B[0m");
-        System.out.println("------------------------------");
+        Account a = new Account(name,tel,password);
+        hashAcc.put(tel,a);
+        System.out.println("\u001B[32m" + "<Create account successfully"+"\u001B[0m");
+        this.pressEnter();
+    }
+
+    public boolean login(int tel,String password){
+        if(this.hashAcc.get(tel)!=null){
+            if(this.hashAcc.get(tel).getPassword().equals(password)) return true;
+        }
+        return false;
     }
 
     public String nextLine(){
@@ -34,6 +46,46 @@ public class Sever {
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
     }
+    public void pressEnter(){
+        System.out.println("\u001B[33m"+"Press Enter key to continue..."+"\u001B[0m");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
+    }
+    public void menuA(){
+        System.out.println("1.Create account");
+        System.out.println("2.Login account");
+        System.out.println("3.Exit");
+    }
+    public void menuB(){
+        System.out.println("1.List product");
+        System.out.println("2.My cart");
+        System.out.println("3.Convenient card");
+        System.out.println("4.History transaction");
+        System.out.println("5.Logout account");
+    }
+    public void menuB1(){
+        System.out.println("1.Choose product");
+        System.out.println("2.Exit");
+    }
+    public void menuB2(){
+        System.out.println("1.Change quantify");
+        System.out.println("2.Order");
+        System.out.println("3.Clear cart");
+        System.out.println("4.Exit");
+    }
+    public void menuB3A(){
+        System.out.println("1.Create convenient card");
+        System.out.println("2.Exit");
+    }
+    public void menuB3B(){
+        System.out.println("1.Check balance");
+        System.out.println("2.");
+    }
+
     public static void setLastAcc(int n){
         Sever.lastAcc=n;
     }
